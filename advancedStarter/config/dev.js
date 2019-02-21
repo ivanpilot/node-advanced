df97255bc2c36fdb69fdee1bf6a4a93963474277
Ivan Pilot <ivan@localhost.localdomain>
2019-02-21 20:20:46 +0000

    adding picture to blog post and being stored inside aws s3 bucket

--- advancedStarter/config/dev.js
+++ advancedStarter/config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://ivan:password1@ds159624.mlab.com:59624/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAI6CQF2RE2ONRLXBQ',
+  secretAccessKey: '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'
 };
